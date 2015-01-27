4c90caba6779dea99ecc828e78583474a5bbab98
Josh Andrews <jtandr3ws37@gmail.com>
2015-01-27 14:42:20 -0800

    generate sites controller w/ index, about & contact views. navigation partial added to layouts.

--- geotrigger_app/config/routes.rb
+++ geotrigger_app/config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
+
+  get 'sites/index'
+
+  get 'sites/about'
+
+  get 'sites/contact'
+
+	root to: "sites#index"
   
-  get 'sessions/new'
+  # get 'sessions/new'
 
-  get 'users/index'
+  # get 'users/index'
 
-  get 'users/show'
+  # get 'users/show'
 
-  get 'users/new'
+  # get 'users/new'
 
-  get 'users/edit'
+  # get 'users/edit'
 
   resources :users, except: [:index, :new]
 
