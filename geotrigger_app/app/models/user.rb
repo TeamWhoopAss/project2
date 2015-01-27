a26e28cacc3c27fdbe59270338300012772ee10b
Josh Andrews <jtandr3ws37@gmail.com>
2015-01-26 23:34:21 -0800

    require bcrypt in user model, add si has_secure_password, validations to user model.

--- geotrigger_app/app/models/user.rb
+++ geotrigger_app/app/models/user.rb
@@ -1,2 +1,19 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
+	has_secure_password
+  
+  validates_confirmation_of :email
+  validates_presence_of :password_confirmation
+  validates_presence_of :email_confirmation
+
+  validates_uniqueness_of :email
+  validates_presence_of :email
+
+  def self.confirm(email_param, password_param)
+    user = User.find_by({email: email_param})
+    if user
+      user.authenticate(password_param)
+    end
+  end
+  
 end
