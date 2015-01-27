4c379e000ddb14e0c39e1e82ad6e12936053a014
Josh Andrews <jtandr3ws37@gmail.com>
2015-01-27 14:00:15 -0800

    geocode by user location and ip address added to user model.

--- geotrigger_app/app/models/user.rb
+++ geotrigger_app/app/models/user.rb
@@ -12,8 +12,14 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email
   validates_presence_of :email
 
+  # Geocodes location by address
   geocoded_by :full_street_address
   after_validation :geocode, :if => :address_changed?
+
+  # Geocode user location by IP address
+  geocoded_by :ip_address,
+  	:latitude => :lat, :longitude => :lon
+  after_validation :gecode
 
   def search_results
   	results = Geocoder.search("Golden Gate Park, San Francisco, CA")
