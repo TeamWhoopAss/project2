1f81c7b433963069594cfc4686f48d167a798487
Josh Andrews <jtandr3ws37@gmail.com>
2015-01-28 12:21:43 -0800

    create migration DropLocations to drop Locations table from db; schema.rb up to date.

--- geotrigger_app/db/schema.rb
+++ geotrigger_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150127194805) do
+ActiveRecord::Schema.define(version: 20150128201819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,14 +27,6 @@ ActiveRecord::Schema.define(version: 20150127194805) do
     t.datetime "expiry"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
-  end
-
-  create_table "locations", force: :cascade do |t|
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
