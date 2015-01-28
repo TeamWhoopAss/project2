20293264fd2b8a28e8d906a802821d3e61563591
Mark Daniels <mark_daniels1982@hotmail.com>
2015-01-27 19:58:45 -0800

    Build testing suite for user and password

--- geotrigger_app/spec/models/user_spec.rb
+++ geotrigger_app/spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+arequire 'rails_helper'
 
 RSpec.describe User, :type => :model do
   describe "validation" do
@@ -59,5 +59,165 @@ describe "email" do
       expect(user.save).to be(false)
     end
 
-    it "should enforce persence of email_confirmation" do
-      @valid_params[:email_confirmation]
+    it "should enforce presence of email_confirmation" do
+      @valid_params[:email_confirmation] = nil
+      user = User.new(@valid_params)
+
+      expect(user.save).to be(false)
+    end
+
+    it "should be valid format" do
+      @valid_params[:email] = @valid_params[:email_confirmation] = "notValid"
+
+      expect(user.save).to be(false)
+    end
+
+  end
+
+  describe "on updates with and invalid email" do
+
+    before(:example) do
+      @valid_params = {
+                        email: "MarkMark@MarkMark.com",
+                        email_confirmation: "MarkMark@MarkMark.com",
+                        password: "MarkMark",
+                        password_confirmation: "MarkMark",
+                      }
+      @valid_user = User.create(@valid_params)
+    end
+
+    after(:example) do
+      @valid_user.destroy()
+    end
+
+    it "should be confirmed" do
+      @valid_params[:email_confirmation] = "notMatching"
+      is_updated = @valid_user.update_attributes(@valid_params)
+
+      expect(is_updated).to be(false)
+    end
+
+    it "should enforce presence of email_confirmation if :email_changed" do
+      @valid_params[:email] = "changedemail@email.com"
+      @valid_params[:email_confirmation] = ""
+      is_updated = @valid_user.update_attributes(@valid_params)
+
+      expect(is_updated).to be(false)
+    end
+
+    it "should have a valid format" do
+      @valid_params[:email] = @valid_params[:email_confirmation] = "notValid"
+      is_updated = @valid_user.update_attributes(@valid_params)
+
+      expect(is_updated).to be(false)
+    end
+  end
+
+
+end
+
+describe "password" do
+
+  describe "on create" do
+
+    it "should be present" do
+      @valid_params[:password] = @valid_params[:password_confirmation] = nil
+      user = User.new(@valid_params)
+
+      expect(user.save).to be(false)
+    end
+
+    it "should be confirmed" do
+      @valid_params[:password_confirmation] = "notMatching"
+      user = User.new(@valid_params)
+
+      expect(user.save).to be(false)
+    end
+
+    it "should enforce presence of password_confirmation" do
+      @valid_params[:password_confirmation] = nil
+      user = User.new(@valid_params)
+
+      expect(user.save).to be(false)
+    end
+
+    it "should be a valid length" do
+      @valid_params[:password] = @valid_params[:password_confirmation] = "small"
+      user = User.new(@valid_params)
+
+      expect(user.save).to be(false)
+    end
+
+    it "should have a vaild format" do
+      @valid_params[:password] = @valid_params[:password_confirmation] = "poorformat"
+      user = User.new(@valid_params)
+
+      expect(user.save).to be(false)
+    end
+
+    describe "on where password is invalid" do
+
+      it "should not require confirmation" do
+        valid_params = {
+          email: "MarkMark@MarkKicksTheFunkyBeats.com",
+          email_confirmation: "MarkMark@MarkKicksTheFunkyBeats.com",
+          password: "MarkMark",
+          password_confirmation: "MarkMark"
+        }
+
+    valid_user = User.create(valid_params)
+    valid_user = User.find(valid_user.id)
+
+    expect(valid_user.reload.update!(first_name: "Mark")).to be_truthy
+
+  end
+
+end
+
+
+  describe "on updates where password is valid" do
+
+    before(:example) do
+      @valid_params = {
+         email: "Mark@MarkIsTheRockingestRocker.com",
+         email_confirmation: "Mark@MarkIsTheRockingestRocker.com",
+         password: "MarkMark",
+         password_confirmation: "MarkMark"
+      }
+
+@valid_user = User.create(@valid_params)
+end
+
+after(:example) do
+  @valid_user.destroy()
+end
+
+it "should be confirmed" do
+  @valid_params[:password_confirmation] = "notMatching"
+  is_updated = @valid_user.update_attributes(@valid_params)
+
+  expect(is_updated).to be(false)
+end
+
+it "should have valid length" do
+  @valid_params[:password] = @valid_params[:password_confirmation] = "small"
+  is_updated = @valid_user.update_attributes(@valid_params)
+
+  expect(is_updated).to be(false)
+end
+
+it "should be valid format" do
+  @valid_params[:password] = valid_params[:password_confirmation] = "poorformat"
+  is_updated = @valid_user.update_attributes(@valid_params)
+
+  expect(is_updated).to be(false)
+     end
+
+   end
+
+  end
+
+ end
+
+end
+
