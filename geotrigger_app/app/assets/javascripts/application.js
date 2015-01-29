061f116d4fb921ad6ea06edb81bfa7453201f99b
Josh Andrews <jtandr3ws37@gmail.com>
2015-01-29 10:20:50 -0800

    Basic landin page and sites layout setup.

--- geotrigger_app/app/assets/javascripts/application.js
+++ geotrigger_app/app/assets/javascripts/application.js
@@ -10,8 +10,12 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require mapbox.js
+//= require animatescroll.js
+//= require bootstrap.min.js
+//= require retina.min.js
+//= require scripts.js
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require mapbox.js
